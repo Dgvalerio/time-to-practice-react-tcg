e01a9cb6e2e222f00a6d3d1fa002650fefba6bd4
Davi Gonçalves Valério <al.davigvalerio@gmail.com>
2021-07-15 08:50:32 -0300

    Class 91 - Adding a re-usable "Button"

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,20 +1,25 @@
 import React from 'react';
+import Card from '../UI/Card';
+
+import classes from './AddUser.module.css';
+import Button from '../UI/Button';
 
 const AddUser = () => {
-
   const addUserHandler = (event) => {
     event.preventDefault();
-  }
+  };
 
   return (
-    <form onSubmit={addUserHandler}>
-      <label form="username">Username</label>
-      <input id="username" type="text" />
-      <label form="age">Age (Years)</label>
-      <input id="age" type="number" />
-      <button type="submit">Add User</button>
-    </form>
-  )
+    <Card className={classes.input}>
+      <form onSubmit={addUserHandler}>
+        <label form="username">Username</label>
+        <input id="username" type="text" />
+        <label form="age">Age (Years)</label>
+        <input id="age" type="number" />
+        <Button type="submit">Add User</Button>
+      </form>
+    </Card>
+  );
 };
 
 export default AddUser;
