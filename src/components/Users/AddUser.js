84d86b9afb6aae00f8a0c0403daab7bc3a31f272
Davi Gonçalves Valério <al.davigvalerio@gmail.com>
2021-07-15 08:54:49 -0300

    Class 92 - OnChange Inputs

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,21 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Card from '../UI/Card';
 
 import classes from './AddUser.module.css';
 import Button from '../UI/Button';
 
 const AddUser = () => {
+  const [username, setUsername] = useState('');
+  const [age, setAge] = useState(0);
+
   const addUserHandler = (event) => {
     event.preventDefault();
+
+    console.log(username, age);
   };
+
+  const usernameChangeHandler = ({ target }) => setUsername(target.value);
+
+  const ageChangeHandler = ({ target }) => setAge(target.value);
 
   return (
     <Card className={classes.input}>
       <form onSubmit={addUserHandler}>
         <label form="username">Username</label>
-        <input id="username" type="text" />
+        <input id="username" type="text" onChange={usernameChangeHandler} />
         <label form="age">Age (Years)</label>
-        <input id="age" type="number" />
+        <input id="age" type="number" onChange={ageChangeHandler} />
         <Button type="submit">Add User</Button>
       </form>
     </Card>
