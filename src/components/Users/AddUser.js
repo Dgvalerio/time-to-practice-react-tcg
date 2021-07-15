05ebf8cec0834d899d07898cd69cc0e6a4165df8
Davi Gonçalves Valério <al.davigvalerio@gmail.com>
2021-07-15 08:58:35 -0300

    Class 93 - Basic Validation

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -6,12 +6,19 @@ import Button from '../UI/Button';
 
 const AddUser = () => {
   const [username, setUsername] = useState('');
-  const [age, setAge] = useState(0);
+  const [age, setAge] = useState(1);
 
   const addUserHandler = (event) => {
     event.preventDefault();
 
+    if (username.trim().length === 0 || age.toString().trim().length === 0)
+      return;
+    if (+age < 1)
+      return;
+
     console.log(username, age);
+    setUsername('');
+    setAge(1);
   };
 
   const usernameChangeHandler = ({ target }) => setUsername(target.value);
@@ -22,9 +29,9 @@ const AddUser = () => {
     <Card className={classes.input}>
       <form onSubmit={addUserHandler}>
         <label form="username">Username</label>
-        <input id="username" type="text" onChange={usernameChangeHandler} />
+        <input id="username" type="text" value={username} onChange={usernameChangeHandler} />
         <label form="age">Age (Years)</label>
-        <input id="age" type="number" onChange={ageChangeHandler} />
+        <input id="age" type="number" value={age} onChange={ageChangeHandler} />
         <Button type="submit">Add User</Button>
       </form>
     </Card>
